828ae10bd3b87904a4dbff26c58ace14d91f4eba
G <garyyansen@gmail.com>
2021-12-30 00:07:14 +0800

    Update experience section

--- frontend/src/components/Experiences/Experiences.js
+++ frontend/src/components/Experiences/Experiences.js
@@ -5,16 +5,32 @@ export default function Experiences() {
     return (
         <Container className='exp-section' style={{marginTop:"200px", color:"white"}}>
             <h1>WORK <span>EXPERIENCE</span></h1>
+
             <Row className='job'>
                 <Col xs={12} md={8} lg={9} className='main'>
-                    <h2><strong>TEACHING ASSISTANT</strong></h2>
+                    <h2><strong>CS2102 TEACHING ASSISTANT</strong></h2>
                     <h3 className='subhead'>NATIONAL UNIVERSITY OF SINGAPORE</h3>
-                    <p>Teaching Assistant for CS2030/CS2030S: Programming Methodology II, which delves 
-                       into object-oriented programming, functional programming, and medium scale software
-                       development in Java. I guide students through their weekly assignments and give feedback on their code
-                       submissions.
+                    <ul className='job-desc'>
+                        <li>Teaching Assistant for CS2102: Database Systems</li>
+                        <li>Facilitate the teaching of Relational Algebra, ER Diagram, SQL, 
+                            Functional Dependencies and Normalization techniques.</li>
+                    </ul>
+                </Col>
+                <Col xs={12} md={4} lg={3} className='period'>
+                    January 2022 - Present
+                </Col>
+            </Row>
 
-                    </p>
+            <Row className='job'>
+                <Col xs={12} md={8} lg={9} className='main'>
+                    <h2><strong>CS2030 TEACHING ASSISTANT</strong></h2>
+                    <h3 className='subhead'>NATIONAL UNIVERSITY OF SINGAPORE</h3>
+                    <ul className='job-desc'>
+                        <li>Teaching Assistant for CS2030: Programming Methodology II</li>
+                        <li>Facilitated the teaching of object-oriented programming, functional programming, and medium scale software
+                       development in Java.</li>
+                        <li>Conducted weekly lessons and gave feedback on their weekly code submissions</li>
+                    </ul>
                 </Col>
                 <Col xs={12} md={4} lg={3} className='period'>
                     August 2021 - Present
@@ -25,11 +41,12 @@ export default function Experiences() {
                 <Col xs={12} md={8} lg={9} className='main'>
                     <h2><strong>DATA ANALYST INTERN</strong></h2>
                     <h3 className='subhead'>RUSHOWL SINGAPORE</h3>
-                    <p>
-                        Analysed the geospatial features of Singapore and explore the correlations with commute experience and
-                        daily operations. Conducted research on the commute pattern of platform users and generate service
-                        personalization and improve user satisfaction.
-                    </p>
+                    <ul className='job-desc'>
+                        <li>Analysed the geospatial features of Singapore and explored the correlations with commute experience and
+                        daily operations</li>
+                        <li>Conducted research on the commute pattern of platform users and generate service
+                        personalization and improve user satisfaction.</li>
+                    </ul>
                 </Col>
                 <Col xs={12} md={4} lg={3} className='period'>
                     June 2021 - August 2021
@@ -40,13 +57,14 @@ export default function Experiences() {
                 <Col xs={12} md={8} className='main'>
                     <h2><strong>ANALYTICS INTERN</strong></h2>
                     <h3 className='subhead'>NATIONAL UNIVERSITY OF SINGAPORE</h3>
-                    <p>
-                        Performed cleaning and processing of various maritime-related data
-                        (AIS, Vessels Stoppage Data) using Python. Analysed and conducted visualisations (vessels trajectories) to detect and remove
-                        noisy entries. Organised the datasets (splitting the datasets based on static and dynamic 
-                        information) for use as input to predictive machine learning.
-
-                    </p>
+                    <ul className='job-desc'>
+                        <li>Performed cleaning and processing of various maritime-related data
+                        (AIS, Vessels Stoppage Data) using Python.</li>
+                        <li>Analysed and conducted visualisations (vessels trajectories) to detect and remove
+                        noisy entries.</li>
+                        <li>Organised the datasets (splitting the datasets based on static and dynamic 
+                        information) for use as input to predictive machine learning.</li>
+                    </ul>
                 </Col>
                 <Col xs={12} md={4} className='period'>
                     December 2020 - January 2021
