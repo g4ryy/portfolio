2e5e886475dbdf9b99ff0543c4a4f06571f2b3ae
G <garyyansen@gmail.com>
2022-01-11 15:44:33 +0800

    Update resume

--- frontend/src/components/AboutMe/About.js
+++ frontend/src/components/AboutMe/About.js
@@ -14,16 +14,15 @@ export default function About() {
           I am currently a penultimate year undergraduate at the National
           University of Singapore (NUS), pursuing a double major in Data Science
           and Computer Science. I have a strong interest in Software
-          Engineering, as well as Artificial Intelligence and Machine Learning.
-          My career aspirations lie in the intersection between these fields,
-          and I hope to be able to develop prominent technological solutions
-          that can utilize the data in today's world to effect change.
+          Engineering and Artificial Intelligence. My career aspirations lie in the intersection between these fields,
+          and I hope to be able to develop useful technological solutions
+          that can utilize the data in today's world to benefit people's lives.
         </p>
 
         <p>
           With a strong desire to learn and a forthcoming character, I am
           determined to learn new skills as well as hone existing ones whilst
-          working with others to achieve common goals and exceptional results.
+          working with others to achieve common goals.
         </p>
       </Container>
 
